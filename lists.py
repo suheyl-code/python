5e255df5d1a795076d913e9d2f3af05eb0ba5121
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-18 01:03:29 +0300

    lists updated

--- lists.py
+++ lists.py
@@ -4,8 +4,28 @@ chars = 0
 for animal in animals:
     chars += len(animal)
 
-print(f"Total Characters in {animals}: {chars} / average length: {chars/len(animals)}")
+print(
+    f"Total Characters in {animals}: {chars} / average length: {chars/len(animals)}")
 
 
+# enumeration
 for index, animal in enumerate(animals):
     print(f"{index + 1}: {animal}")
+
+
+# list comprehension
+result1 = []
+for x in range(1, 11):
+    result1.append(x*7)
+
+# But better with list comprehension
+result2 = [x*7 for x in range(1, 11)]
+
+print(result1, result2)
+
+languages = ['Ruby', 'Python', 'C', 'C#', 'C++', 'Javascript']
+x = [len(language) for language in languages]
+print(x)
+
+odd = [ number for number in range(0,101) if number % 3 == 0]
+print(odd)
