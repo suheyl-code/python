60b8d78552e7d458f976debd0a122e12f68638b6
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-17 18:51:08 +0300

    new file added!

---
+++ Blockchain/blockchain.py
@@ -0,0 +1,26 @@
+import datetime
+import hashlib
+from itertools import chain
+import json
+from flask import Flask, jsonify
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+
+bc = Blockchain()
+print(bc.get_previous_block())
