139a54f02a05de1831dd37113cfa662e98df2c8f
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-18 15:34:02 +0300

    classes2 updated

--- classes2.py
+++ classes2.py
@@ -6,5 +6,10 @@ class Dog:
     def talk(self):
         print(f"{self.name}: Can't talk, sorry!")
 
+    def __str__(self):
+        return f"This is a Dog class object <{self.name}>"
+
+
 js = Dog("js")
 js.talk()
+print(js)   # this deals with __str__
