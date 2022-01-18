53df6fe1d2ab4d5c974f766a72e0970eaba56d50
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-18 17:08:10 +0300

    new file added + updates

---
+++ classes4.py
@@ -0,0 +1,24 @@
+from importlib.resources import Package
+
+
+class Repository:
+    def __init__(self):
+        self.packages = {}
+        self.number = 1
+
+    def add_package(self, package):
+        self.packages[self.number] = package
+        self.number += 1
+
+    def total_size(self):
+        result = 0
+        for key, value in self.packages.items():
+            result += len(value)
+        return result
+
+
+j = Repository()
+j.add_package('jangol')
+j.add_package('bipoo')
+print(j.packages)
+print(j.total_size())
