2f1704e0074cfe0acec0426f688de2272a89c370
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-19 14:20:56 +0300

    os module updated

--- os-module.py
+++ os-module.py
@@ -1,7 +1,20 @@
+import datetime
 # 'os' module is for working with Filesystem on Operating System
 import os
+
+# Showing absolute path
+print(os.path.abspath("README.txt"))
 
 # To remove a file, if exists:
 if os.path.exists("text.txt"):
     os.rename("text.txt", "text-renamed.txt")
     os.remove("text-renamed.txt")
+
+# Get size in bytes
+print(os.path.getsize("README.md"))
+
+# shows the unix type 'timestamp' of file created time in seconds
+print(os.path.getmtime("README.md"))
+# to make timestamp human readable
+timestamp = os.path.getmtime("README.md")
+print(datetime.datetime.fromtimestamp(timestamp))
