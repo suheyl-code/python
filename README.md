5a2862171d8a6a14b1dfc6b839bdc723a1da0e5c
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-19 11:18:24 +0300

    new file added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python
 Python Practice Sheet
+It Consists of several files.
---
+++ write-files.py
@@ -0,0 +1,3 @@
+# a stands for 'append', w for 'write' to a new file
+with open("README.md", "a") as file:
+    file.write("It Consists of several files.\n")
